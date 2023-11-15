cc12d990c4fcc1a1ab9579343e5dd445cd58e221
unknown <windaryanti1709@gmail.com>
2023-11-15 07:52:13 +0700

    saya commit

---
+++ tugas/windaaryanti1a.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main() {
+	// Menampilkan NIM, NAMA, dan JURUSAN
+	printf("NIM: 2341039\n");
+	printf("Nama: Winda Aryanti");
+	printf("Jurusan: Teknik informatika");
+	
+	return 0;
+}
