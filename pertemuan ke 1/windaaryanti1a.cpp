abb62b3d946a82bd7132a1b42b3ef4bc3119c590
unknown <windaaryanti1709@gmail.com>
2023-11-23 23:38:37 +0700

    saya commit

---
+++ pertemuan ke 1/windaaryanti1a.cpp
@@ -0,0 +1,9 @@
+#include "stdio.h"
+
+int main()
+{
+	printf(" nama     : Winda Aryanti. \n");
+	printf(" nim \t   : 2341039. \n");
+	printf(" jurursan  : teknik informatika. \n");
+	return 0;
+}
---
+++ pertemuan ke 1/windaaryanti1b.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+ // Deklarasi variabel
+ int panjang = 10;
+ int lebar = 7;
+ int luas;
+
+ // Menghitung luas persegi panjang
+ luas = panjang * lebar;
+
+ // Menampilkan luas persegi panjang
+ printf("Luas persegi panjang: %d cm persegi\n", luas);
+
+ return 0;
+}
